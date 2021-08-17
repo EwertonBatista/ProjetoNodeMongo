f417b346d3edd7aaeb1007d8ba5b683a5246fef8
Ewerton Batista <ewerton13batista@gmail.com>
2021-08-17 20:21:07 -0300

    Configurando rotas

--- app.js
+++ app.js
@@ -4,20 +4,47 @@
     const app = express();
     const admin = require('./routes/admin')
     const path = require('path')
-    // const mongoose = require('mongoose')
+    const mongoose = require('mongoose')
+    const session = require('express-session')
+    const flash = require('connect-flash')
 
 // Configurações
-
+    // Sessão
+        app.use(session({
+            secret: "cursodenode",
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+    // Middleware
+        app.use((req,res,next)=>{
+            res.locals.success_msg = req.flash("Success_msg")
+            res.locals.error_msg = req.flash("Error_msg")
+            next();
+        })
     // Express
         app.use(express.urlencoded({ extended: true}))
         app.use(express.json());
     // HandleBars
         app.engine('handlebars', handlebars({defaultLayout: 'main'}));
         app.set('view engine', 'handlebars');
-    // Mongoose
-
+    // Mongoose conexão
+        mongoose.Promise = global.Promise;
+        mongoose.connect('mongodb://localhost/blogapp',
+         {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        }).then(()=>{
+                    console.log(`Conectado ao MongoDB...`)
+                }).catch((err)=>{
+                    console.log(`Erro ao conectar ${err}`)
+        })
     // Public
-        app.use(express.static(path.join(__dirname, 'public')));
+            app.use(express.static(path.join(__dirname, 'public')));
+    
+        
 
 
 // Rotas
