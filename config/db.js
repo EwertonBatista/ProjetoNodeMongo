1a20047fb4a58256233836643c699cb181d26fdf
Ewerton Batista <ewerton13batista@gmail.com>
2021-08-24 00:40:20 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI:"mongodb+srv://EwertonBatista:24071998aA@blogapp.bt9hz.mongodb.net/blogapp?retryWrites=true&w=majority"}
+    module.exports = {mongoURI:"mongodb+srv://EwertonBatista:24071998aA@blogapp.bt9hz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"}
 }else{
     module.exports = {mongoURI:"mongodb://localhost/blogapp"}
 }
