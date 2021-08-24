b4fb9e2ebcba46ed0191f945fa55bcf9c155796a
Ewerton Batista <ewerton13batista@gmail.com>
2021-08-24 16:23:48 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
     module.exports = { mongoURI: "mongodb+srv://EwertonBatista:123321456654@blogapp.bt9hz.mongodb.net/blogapp?retryWrites=true&w=majority" }
 }else{
-    module.exports = {mongoURI:"mongodb://localhost/blogapp"}
+    module.exports = { mongoURI:"mongodb://localhost/blogapp" }
 }
